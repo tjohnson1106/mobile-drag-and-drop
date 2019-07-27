8028f47a25ff8b308663d583ebfa6319a76ae60e
Thomas Johnson <thomasjohnson@Thomass-iMac.local>
2019-07-27 17:37:54 -0400

    created pan responder configuration

--- src/Drag.tsx
+++ src/Drag.tsx
@@ -1,44 +1,121 @@
-import React, { Component } from 'react';
-import { Text, View, FlatList } from 'react-native';
+import React, { Component } from "react";
+import {
+  Text,
+  View,
+  FlatList,
+  StyleSheet,
+  PanResponder,
+  PanResponderInstance
+} from "react-native";
 
 function getRandomColor() {
-	var letters = '0123456789ABCDEF';
-	var color = '#';
-	for (let i = 0; i < 6; i++) {
-		color += letters[Math.floor(Math.random() * 16)];
-	}
-	return color;
+  var letters = "0123456789ABCDEF";
+  var color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
 }
 
 const colorMap = {};
 
 class Drag extends Component {
-	state = {
-		data: Array.from(Array(200), (_, i) => {
-			colorMap[i] = getRandomColor();
-			return i;
-		})
-	};
-	render() {
-		const { data } = this.state;
-		return (
-			<View style={styles.root}>
-				<FlatList
-					style={{ width: '100%' }}
-					data={data}
-					renderItem={({ item }) => '' + item}
-				/>
-			</View>
-		);
-	}
+  state = {
+    data: Array.from(Array(200), (_, i) => {
+      colorMap[i] = getRandomColor();
+      return i;
+    })
+  };
+
+  _panResponder: PanResponderInstance;
+
+  constructor(props) {
+    super(props);
+
+    this._panResponder = PanResponder.create({
+      // Ask to be the responder:
+      onStartShouldSetPanResponder: (evt, gestureState) => true,
+      onStartShouldSetPanResponderCapture: (evt, gestureState) => true,
+      onMoveShouldSetPanResponder: (evt, gestureState) => true,
+      onMoveShouldSetPanResponderCapture: (evt, gestureState) => true,
+
+      onPanResponderGrant: (evt, gestureState) => {
+        // The gesture has started. Show visual feedback so the user knows
+        // what is happening!
+        // gestureState.d{x,y} will be set to zero now
+      },
+      onPanResponderMove: (evt, gestureState) => {
+        console.log(gestureState.moveY);
+        // The most recent move distance is gestureState.move{X,Y}
+        // The accumulated gesture distance since becoming responder is
+        // gestureState.d{x,y}
+      },
+      onPanResponderTerminationRequest: (evt, gestureState) => false,
+      onPanResponderRelease: (evt, gestureState) => {
+        // The user has released all touches while this view is the
+        // responder. This typically means a gesture has succeeded
+      },
+      onPanResponderTerminate: (evt, gestureState) => {
+        // Another component has become the responder, so this gesture
+        // should be cancelled
+      },
+      onShouldBlockNativeResponder: (evt, gestureState) => {
+        // Returns whether this component should block native components from becoming the JS
+        // responder. Returns true by default. Is currently only supported on android.
+        return true;
+      }
+    });
+  }
+
+  render() {
+    const { data } = this.state;
+    return (
+      <View style={styles.root}>
+        <FlatList
+          style={{ width: "100%" }}
+          data={data}
+          renderItem={({ item }) => (
+            <View
+              style={{
+                padding: 16,
+                backgroundColor: colorMap[item],
+                flexDirection: "row"
+              }}
+            >
+              <View {...this._panResponder.panHandlers}>
+                {/* handle */}
+                <Text
+                  style={{
+                    fontSize: 28
+                  }}
+                >
+                  @
+                </Text>
+              </View>
+              <Text
+                style={{
+                  fontSize: 22,
+                  textAlign: "center",
+                  flex: 1
+                }}
+              >
+                {item}
+              </Text>
+            </View>
+          )}
+          keyExtractor={(item) => " " + item}
+        />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-	root: {
-		flex: 1,
-		alignItems: 'center',
-		justifyContent: 'center'
-	}
+  root: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
 
 export default Drag;
